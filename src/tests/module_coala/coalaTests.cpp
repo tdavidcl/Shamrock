522a136a50889479e44735ce902633c0f9551914
Timothée David--Cléris <timothee.davidcleri@gmail.com>
2023-10-26 12:47:37 +0200

    [COALA] add test in shamrock

---
+++ src/tests/module_coala/coalaTests.cpp
@@ -0,0 +1,15 @@
+// -------------------------------------------------------//
+//
+// SHAMROCK code for hydrodynamics
+// Copyright(C) 2021-2023 Timothée David--Cléris <timothee.david--cleris@ens-lyon.fr>
+// Licensed under CeCILL 2.1 License, see LICENSE for more information
+//
+// -------------------------------------------------------//
+
+#include "coala/coala.hpp"
+
+#include "shamtest/shamtest.hpp"
+
+TestStart(Unittest, "coala", coala, 1){
+
+}
