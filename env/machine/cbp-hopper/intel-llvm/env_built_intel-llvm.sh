963c18b155092e079a3286929f5954d6493602d1
Timothée David--Cléris <timothee.davidcleris@proton.me>
2024-04-03 14:50:59 +0200

    [Env] update hopper script

--- env/machine/cbp-hopper/intel-llvm/env_built_intel-llvm.sh
+++ env/machine/cbp-hopper/intel-llvm/env_built_intel-llvm.sh
@@ -12,7 +12,8 @@ function setupcompiler {
         --cuda \
         --cmake-opt="-DCUDA_TOOLKIT_ROOT_DIR=${CUDA_PATH}" \
         --cmake-gen "${CMAKE_GENERATOR}" \
-        --cmake-opt="-DCMAKE_INSTALL_PREFIX=${INTELLLVM_INSTALL_DIR}"
+        --cmake-opt="-DCMAKE_INSTALL_PREFIX=${INTELLLVM_INSTALL_DIR}" \
+        --host-target aarch64
 
     (cd ${INTELLLVM_GIT_DIR}/build && $MAKE_EXEC "${MAKE_OPT[@]}" all libsycldevice)
     (cd ${INTELLLVM_GIT_DIR}/build && $MAKE_EXEC install)
