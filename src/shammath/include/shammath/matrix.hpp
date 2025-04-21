606b49a732491c452ac6f5f6f1049e560e038a5b
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-04-21 12:36:35 +0200

    more doc

--- src/shammath/include/shammath/matrix.hpp
+++ src/shammath/include/shammath/matrix.hpp
@@ -24,19 +24,31 @@
 
 namespace shammath {
 
+    /**
+     * @brief Matrix class based on std::array storage and mdspan
+     * @tparam T the type of the matrix entries
+     * @tparam m the number of rows
+     * @tparam n the number of columns
+     */
     template<class T, int m, int n>
     class mat {
         public:
+        /// The matrix data
         std::array<T, m * n> data;
+
+        /// Get the matrix data as a mdspan
         inline constexpr auto get_mdspan() {
             return std::mdspan<T, std::extents<size_t, m, n>>(data.data());
         }
 
+        /// Access the matrix entry at position (i, j)
         inline constexpr T &operator()(int i, int j) { return get_mdspan()(i, j); }
 
+        /// Check if this matrix is equal to another one
         bool operator==(const mat<T, m, n> &other) { return data == other.data; }
     };
 
+    /// Returns the identity matrix of size n
     template<class T, int n>
     inline constexpr mat<T, n, n> mat_identity() {
         mat<T, n, n> res{};
@@ -44,23 +56,36 @@ namespace shammath {
         return res;
     }
 
+    /**
+     * @brief Vector class based on std::array storage and mdspan
+     * @tparam T the type of the vector entries
+     * @tparam n the number of entries
+     */
     template<class T, int n>
     class vec {
         public:
+        /// The vector data
         std::array<T, n> data;
+
+        /// Get the vector data as a mdspan
         inline constexpr auto get_mdspan() {
             return std::mdspan<T, std::extents<size_t, n>>(data.data());
         }
 
+        /// Get the vector data as a mdspan of a matrix with one column
         inline constexpr auto get_mdspan_mat_col() {
             return std::mdspan<T, std::extents<size_t, n, 1>>(data.data());
         }
+
+        /// Get the vector data as a mdspan of a matrix with one row
         inline constexpr auto get_mdspan_mat_row() {
             return std::mdspan<T, std::extents<size_t, 1, n>>(data.data());
         }
 
+        /// Access the vector entry at position i
         inline constexpr T &operator[](int i) { return get_mdspan()(i); }
 
+        /// Check if this vector is equal to another one
         bool operator==(const vec<T, n> &other) { return data == other.data; }
     };
 
