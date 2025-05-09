6591c5082aa9a6c16150b316348feb4dcb3463b7
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-05-09 19:08:25 +0200

    compute cell aabb node

--- src/shammodels/ramses/src/modules/ComputeCellInfos.cpp
+++ src/shammodels/ramses/src/modules/ComputeCellInfos.cpp
@@ -15,71 +15,124 @@
  */
 
 #include "shammodels/ramses/modules/ComputeCellInfos.hpp"
+#include "shamcomm/logs.hpp"
 #include "shammodels/common/amr/AMRCellInfos.hpp"
+#include "shammodels/ramses/modules/ComputeCellAABB.hpp"
+#include "shamrock/patch/PatchDataField.hpp"
 #include "shamrock/scheduler/SchedulerUtility.hpp"
 
 template<class Tvec, class TgridVec>
 void shammodels::basegodunov::modules::ComputeCellInfos<Tvec, TgridVec>::compute_aabb() {
+    if (false) {
+        StackEntry stack_loc{};
 
-    StackEntry stack_loc{};
+        using MergedPDat = shamrock::MergedPatchData;
 
-    using MergedPDat = shamrock::MergedPatchData;
+        logger::debug_ln("AMR grid", "compute block/cell infos");
 
-    logger::debug_ln("AMR grid", "compute block/cell infos");
+        shamrock::SchedulerUtility utility(scheduler());
 
-    shamrock::SchedulerUtility utility(scheduler());
+        shamrock::ComputeField<Tscal> block_cell_sizes
+            = utility.make_compute_field<Tscal>("aabb cell size", 1, [&](u64 id) {
+                  return storage.merged_patchdata_ghost.get().get(id).total_elements;
+              });
+        shamrock::ComputeField<Tvec> cell0block_aabb_lower
+            = utility.make_compute_field<Tvec>("aabb cell lower", 1, [&](u64 id) {
+                  return storage.merged_patchdata_ghost.get().get(id).total_elements;
+              });
 
-    shamrock::ComputeField<Tscal> block_cell_sizes
-        = utility.make_compute_field<Tscal>("aabb cell size", 1, [&](u64 id) {
-              return storage.merged_patchdata_ghost.get().get(id).total_elements;
-          });
-    shamrock::ComputeField<Tvec> cell0block_aabb_lower
-        = utility.make_compute_field<Tvec>("aabb cell lower", 1, [&](u64 id) {
-              return storage.merged_patchdata_ghost.get().get(id).total_elements;
-          });
+        storage.merged_patchdata_ghost.get().for_each([&](u64 id, MergedPDat &mpdat) {
+            sham::DeviceQueue &q = shamsys::instance::get_compute_scheduler().get_queue();
 
-    storage.merged_patchdata_ghost.get().for_each([&](u64 id, MergedPDat &mpdat) {
-        sham::DeviceQueue &q = shamsys::instance::get_compute_scheduler().get_queue();
+            sham::DeviceBuffer<TgridVec> &buf_block_min = mpdat.pdat.get_field_buf_ref<TgridVec>(0);
+            sham::DeviceBuffer<TgridVec> &buf_block_max = mpdat.pdat.get_field_buf_ref<TgridVec>(1);
 
-        sham::DeviceBuffer<TgridVec> &buf_block_min = mpdat.pdat.get_field_buf_ref<TgridVec>(0);
-        sham::DeviceBuffer<TgridVec> &buf_block_max = mpdat.pdat.get_field_buf_ref<TgridVec>(1);
+            sham::EventList depends_list;
 
-        sham::EventList depends_list;
+            auto acc_block_min = buf_block_min.get_read_access(depends_list);
+            auto acc_block_max = buf_block_max.get_read_access(depends_list);
+            auto bsize         = block_cell_sizes.get_buf(id).get_write_access(depends_list);
+            auto aabb_lower    = cell0block_aabb_lower.get_buf(id).get_write_access(depends_list);
 
-        auto acc_block_min = buf_block_min.get_read_access(depends_list);
-        auto acc_block_max = buf_block_max.get_read_access(depends_list);
-        auto bsize         = block_cell_sizes.get_buf(id).get_write_access(depends_list);
-        auto aabb_lower    = cell0block_aabb_lower.get_buf(id).get_write_access(depends_list);
+            auto e = q.submit(depends_list, [&](sycl::handler &cgh) {
+                Tscal one_over_Nside = 1. / AMRBlock::Nside;
 
-        auto e = q.submit(depends_list, [&](sycl::handler &cgh) {
-            Tscal one_over_Nside = 1. / AMRBlock::Nside;
+                Tscal dxfact = solver_config.grid_coord_to_pos_fact;
 
-            Tscal dxfact = solver_config.grid_coord_to_pos_fact;
+                shambase::parralel_for(
+                    cgh, mpdat.total_elements, "compute cell infos", [=](u32 gid) {
+                        TgridVec lower = acc_block_min[gid];
+                        TgridVec upper = acc_block_max[gid];
 
-            shambase::parralel_for(cgh, mpdat.total_elements, "compute cell infos", [=](u32 gid) {
-                TgridVec lower = acc_block_min[gid];
-                TgridVec upper = acc_block_max[gid];
+                        Tvec lower_flt = lower.template convert<Tscal>() * dxfact;
+                        Tvec upper_flt = upper.template convert<Tscal>() * dxfact;
 
-                Tvec lower_flt = lower.template convert<Tscal>() * dxfact;
-                Tvec upper_flt = upper.template convert<Tscal>() * dxfact;
+                        Tvec block_cell_size = (upper_flt - lower_flt) * one_over_Nside;
 
-                Tvec block_cell_size = (upper_flt - lower_flt) * one_over_Nside;
+                        Tscal res = block_cell_size.x();
 
-                Tscal res = block_cell_size.x();
-
-                bsize[gid]      = res;
-                aabb_lower[gid] = lower_flt;
+                        bsize[gid]      = res;
+                        aabb_lower[gid] = lower_flt;
+                    });
             });
+
+            buf_block_min.complete_event_state(e);
+            buf_block_max.complete_event_state(e);
+            block_cell_sizes.get_buf(id).complete_event_state(e);
+            cell0block_aabb_lower.get_buf(id).complete_event_state(e);
         });
 
-        buf_block_min.complete_event_state(e);
-        buf_block_max.complete_event_state(e);
-        block_cell_sizes.get_buf(id).complete_event_state(e);
-        cell0block_aabb_lower.get_buf(id).complete_event_state(e);
-    });
+        logger::raw_ln("block_cell_sizes", ":");
+        block_cell_sizes.field_data.for_each([&](u64 id, PatchDataField<Tscal> &pdf) {
+            logger::raw_ln(pdf.get_buf().copy_to_stdvec());
+        });
 
-    storage.cell_infos.set(
-        CellInfos<Tvec, TgridVec>{std::move(block_cell_sizes), std::move(cell0block_aabb_lower)});
+        logger::raw_ln("cell0block_aabb_lower", ":");
+        cell0block_aabb_lower.field_data.for_each([&](u64 id, PatchDataField<Tvec> &pdf) {
+            logger::raw_ln(pdf.get_buf().copy_to_stdvec());
+        });
+
+        storage.cell_infos.set(CellInfos<Tvec, TgridVec>{
+            std::move(block_cell_sizes), std::move(cell0block_aabb_lower)});
+
+    } else {
+
+        // will be filled by NodeComputeCellAABB
+        auto spans_block_cell_sizes = std::make_shared<shamrock::solvergraph::Field<Tscal>>(
+            1, "vel_dust", "\\mathbf{v}_{\\rm dust}");
+        auto spans_cell0block_aabb_lower = std::make_shared<shamrock::solvergraph::Field<Tvec>>(
+            1, "vel_dust", "\\mathbf{v}_{\\rm dust}");
+
+        NodeComputeCellAABB<Tvec, TgridVec> node{
+            AMRBlock::Nside, solver_config.grid_coord_to_pos_fact};
+
+        node.set_edges(
+            storage.block_counts_with_ghost,
+            storage.spans_block_min,
+            storage.spans_block_max,
+            spans_block_cell_sizes,
+            spans_cell0block_aabb_lower);
+        node.evaluate();
+
+        logger::raw_ln(" --- dot:\n" + node.get_dot_graph());
+        logger::raw_ln(" --- tex:\n" + node.get_tex());
+
+        shamrock::ComputeField<Tscal> block_cell_sizes     = spans_block_cell_sizes->extract();
+        shamrock::ComputeField<Tvec> cell0block_aabb_lower = spans_cell0block_aabb_lower->extract();
+
+        logger::raw_ln("block_cell_sizes", ":");
+        block_cell_sizes.field_data.for_each([&](u64 id, PatchDataField<Tscal> &pdf) {
+            logger::raw_ln(pdf.get_buf().copy_to_stdvec());
+        });
+
+        logger::raw_ln("cell0block_aabb_lower", ":");
+        cell0block_aabb_lower.field_data.for_each([&](u64 id, PatchDataField<Tvec> &pdf) {
+            logger::raw_ln(pdf.get_buf().copy_to_stdvec());
+        });
+
+        storage.cell_infos.set(CellInfos<Tvec, TgridVec>{
+            std::move(block_cell_sizes), std::move(cell0block_aabb_lower)});
+    }
 }
 
 template class shammodels::basegodunov::modules::ComputeCellInfos<f64_3, i64_3>;
