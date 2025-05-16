b8f4773500c5b6adfd8dfc3d1f486d088b4b6b79
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-05-16 20:37:02 +0200

    change split for insertion

--- exemples/sph_weak_scale_test.py
+++ exemples/sph_weak_scale_test.py
@@ -65,7 +65,7 @@ model.resize_simulation_box(bmin, bmax)
 
 setup = model.get_setup()
 gen = setup.make_generator_lattice_hcp(dr, bmin, bmax)
-setup.apply_setup(gen, insert_step=scheduler_split_val // 4)
+setup.apply_setup(gen, insert_step=scheduler_split_val)
 
 
 xc, yc, zc = model.get_closest_part_to((0, 0, 0))
