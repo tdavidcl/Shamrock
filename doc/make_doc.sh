9203a49ddcd8482f3312fce53dde005e9ab06f35
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-06-23 14:06:55 +0200

    multiple doxygen attempts

--- doc/make_doc.sh
+++ doc/make_doc.sh
@@ -1,7 +1,20 @@
-set -e
-
 cd doxygen
-doxygen dox.conf
+for i in {1..3}
+do
+    doxygen dox.conf
+    if [ $? -eq 0 ]
+    then
+        break
+    fi
+    echo "Doxygen failed, retrying ($i/3)"
+done
+if [ $i -eq 3 ]
+then
+    echo "Doxygen failed three times, giving up."
+    exit 1
+fi
+
+set -e
 
 cd ../mkdocs
 cd docs/assets/figures
