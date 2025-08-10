acc1c34771288e2fec69dc07748f7113b6f40d61
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-08-10 18:32:34 +0200

    WIP

---
+++ doc/sphinx/examples/math/run_paving_ghost_layer.py
@@ -0,0 +1,254 @@
+"""
+Paving functions
+================
+
+This example showcase how to use the paving functions to generate ghost layers.
+
+The complex thing is that we want to intersect the current data with the ghost layer,
+but we do not want to modify the original buffer. As a result we perform the intersection
+is a space that is transformed by the paving function, perform the intersection in that space,
+and then map back the result to the original space.
+
+Formally speaking
+
+Ghost layer = f(patch) V box
+            = f( f_inv(f(patch) V box) )
+            = f( patch V f_inv(box) )
+
+Where V denote a ghost layer intersection.
+"""
+
+# sphinx_gallery_multi_image = "single"
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import shamrock
+
+# %%
+# Set box size
+
+box_size_x = 2.0
+box_size_y = 2.0
+
+# %%
+# Particle set
+
+color_set = [
+    "blue",
+    "red",
+    "green",
+    "black",
+    "orange",
+    "purple",
+    "brown",
+    "pink",
+    "gray",
+    "olive",
+    "cyan",
+    "magenta",
+    "yellow",
+    "teal",
+    "navy",
+    "gold",
+    "lime",
+    "indigo",
+    "maroon",
+    "turquoise",
+]
+
+x_set = np.linspace(0.2, box_size_x - 0.2, 3)
+y_set = np.linspace(0.2, box_size_y - 0.2, 3)
+
+parts = []
+i = 0
+for x in x_set:
+    for y in y_set:
+        parts.append({"x": x, "y": y, "color": color_set[i % len(color_set)]})
+        i += 1
+
+
+# %%
+# Utility to plot the paving function
+
+margin = 0.1
+
+
+def add_rect(x, y, w, h):
+    plt.gca().add_patch(
+        plt.Rectangle(
+            (x + margin, y + margin),
+            w - 2 * margin,
+            h - 2 * margin,
+            alpha=0.5,
+            fill=True,
+            facecolor="grey",
+            linewidth=2,
+        )
+    )
+
+
+def add_rect_aabb(aabb):
+    add_rect(
+        aabb.lower[0], aabb.lower[1], aabb.upper[0] - aabb.lower[0], aabb.upper[1] - aabb.lower[1]
+    )
+
+
+def ghost_intersect(part, box_to_intersect):
+    x, y, z = part["x"], part["y"], 0.0
+
+    part_aabb = shamrock.math.AABB_f64_3((x - 0.3, y - 0.3, 0.0), (x + 0.3, y + 0.3, 0.0))
+
+    intersect = box_to_intersect.get_intersect(part_aabb)
+
+    _x, _y, _z = intersect.upper
+    _z = 1.0  # we want to be sure that the volume is not null but the z is not used
+    intersect.upper = (_x, _y, _z)
+
+    print(part_aabb, box_to_intersect, intersect, intersect.get_volume())
+
+    return intersect.is_volume_not_null()
+
+
+def plot_paving_function(pav_func, pav_func_name, shear_x=0.0):
+
+    plt.figure()
+
+    for i in range(-2, 3):
+        for j in range(-3, 4):
+            if i == 0 and j == 0:
+                continue
+
+            box_to_intersect = shamrock.math.AABB_f64_3(
+                (0.0, 0.0, 0.0), (box_size_x, box_size_y, 0.0)
+            )
+
+            box_to_intersect_inv_mapped = pav_func.f_aabb_inv(box_to_intersect, i, j, 0)
+
+            add_rect_aabb(box_to_intersect_inv_mapped)
+
+    for part in parts:
+        x, y, z = (part["x"], part["y"], 0.0)
+        plt.scatter(x, y, color=part["color"])
+
+    plt.title(f"Paving function: {pav_func_name} - 1. Inverse map current sim box")
+    plt.xlabel("x")
+    plt.ylabel("y")
+
+    plt.figure()
+
+    for i in range(-2, 3):
+        for j in range(-3, 4):
+            if i == 0 and j == 0:
+                continue
+
+            # if(not (i == -1 and j == -1)):
+            # continue
+
+            box_to_intersect = shamrock.math.AABB_f64_3(
+                (0.0, 0.0, 0.0), (box_size_x, box_size_y, 0.0)
+            )
+
+            box_to_intersect_inv_mapped = pav_func.f_aabb_inv(box_to_intersect, i, j, 0)
+
+            add_rect_aabb(box_to_intersect_inv_mapped)
+
+            for part in parts:
+                x, y, z = (part["x"], part["y"], 0.0)
+                if ghost_intersect(part, box_to_intersect_inv_mapped):
+                    print("scatter")
+                    plt.scatter(x, y, color=part["color"])
+
+    plt.title(f"Paving function: {pav_func_name} - 2. Inverse ghost layer with inverse map")
+    plt.xlabel("x")
+    plt.ylabel("y")
+
+    plt.figure()
+
+    for i in range(-2, 3):
+        for j in range(-3, 4):
+            if i == 0 and j == 0:
+                continue
+
+            # if(not (i == -1 and j == -1)):
+            # continue
+
+            box_to_intersect = shamrock.math.AABB_f64_3(
+                (0.0, 0.0, 0.0), (box_size_x, box_size_y, 0.0)
+            )
+
+            box_to_intersect_inv_mapped = pav_func.f_aabb_inv(box_to_intersect, i, j, 0)
+            box_to_intersect_mapped = pav_func.f_aabb(box_to_intersect, i, j, 0)
+
+            add_rect_aabb(box_to_intersect_mapped)
+
+            for part in parts:
+                x, y, z = (part["x"], part["y"], 0.0)
+
+                if ghost_intersect(part, box_to_intersect_inv_mapped):
+                    x, y, z = pav_func.f((x, y, 0.0), i, j, 0)
+                    plt.scatter(x, y, color=part["color"])
+
+    for part in parts:
+        x, y, z = (part["x"], part["y"], 0.0)
+        plt.scatter(x, y, color=part["color"])
+
+    plt.title(f"Paving function: {pav_func_name} - 3. Map back the resulting ghost layer")
+    plt.xlabel("x")
+    plt.ylabel("y")
+
+
+# %%
+# Testing the paving functions
+# ----------------------------
+
+# %%
+# Periodic paving function
+
+plot_paving_function(
+    shamrock.math.paving_function_general_3d(
+        (box_size_x, box_size_y, 0.0), (box_size_x / 2.0, box_size_y / 2.0, 0.0), True, True, True
+    ),
+    "Periodic box",
+)
+
+
+# %%
+# Periodic & reflective paving function
+
+plot_paving_function(
+    shamrock.math.paving_function_general_3d(
+        (box_size_x, box_size_y, 0.0), (box_size_x / 2.0, box_size_y / 2.0, 0.0), False, True, True
+    ),
+    "reflective in x periodic in y",
+)
+
+
+# %%
+# Fully reflective paving function
+
+plot_paving_function(
+    shamrock.math.paving_function_general_3d(
+        (box_size_x, box_size_y, 0.0), (box_size_x / 2.0, box_size_y / 2.0, 0.0), False, False, True
+    ),
+    "Fully reflective",
+)
+
+
+# %%
+# Periodic & reflective paving function with shear
+
+plot_paving_function(
+    shamrock.math.paving_function_general_3d_shear_x(
+        (box_size_x, box_size_y, 0.0),
+        (box_size_x / 2.0, box_size_y / 2.0, 0.0),
+        False,
+        True,
+        True,
+        0.3,
+    ),
+    "reflective in x periodic in y with shear",
+    shear_x=0.3,
+)
+
+plt.show()
---
+++ src/shammodels/ramses/src/modules/GenerateGhostLayer.cpp
@@ -0,0 +1,95 @@
+// -------------------------------------------------------//
+//
+// SHAMROCK code for hydrodynamics
+// Copyright (c) 2021-2025 Timothée David--Cléris <tim.shamrock@proton.me>
+// SPDX-License-Identifier: CeCILL Free Software License Agreement v2.1
+// Shamrock is licensed under the CeCILL 2.1 License, see LICENSE for more information
+//
+// -------------------------------------------------------//
+
+/**
+ * @file GenerateGhostLayer.cpp
+ * @author Timothée David--Cléris (tim.shamrock@proton.me)
+ * @brief
+ */
+
+#include "shambase/assert.hpp"
+#include "shambase/exception.hpp"
+#include "shammath/AABB.hpp"
+#include "shammath/paving_function.hpp"
+#include "shammodels/ramses/modules/GenerateGhostLayer.hpp"
+#include <stdexcept>
+
+namespace {
+
+    using namespace shammodels::basegodunov::modules;
+
+    template<class TgridVec>
+    shammath::paving_function_general_3d<TgridVec>
+    get_paving(GhostLayerGenMode mode, shammath::AABB<TgridVec> sim_box) {
+
+        TgridVec box_size   = sim_box.max - sim_box.min;
+        TgridVec box_center = (sim_box.max + sim_box.min) / 2;
+
+        SHAM_ASSERT(sim_box.is_volume_not_null());
+
+        { // check that rebuildind the AABB from size and center gives the same AABB
+            shammath::AABB<TgridVec> new_box
+                = {box_center - box_size / 2, box_center + box_size / 2};
+            if (new_box != sim_box) {
+                shambase::throw_with_loc<std::invalid_argument>(
+                    "Rebuilding AABB from size and center gives a different AABB");
+            }
+        }
+
+        return shammath::paving_function_general_3d<TgridVec>{
+            box_size,
+            box_center,
+            mode.ghost_type_x == GhostType::Periodic,
+            mode.ghost_type_y == GhostType::Periodic,
+            mode.ghost_type_z == GhostType::Periodic};
+    }
+
+} // namespace
+
+/**
+
+Ok this is the hard bit.
+
+A ghost layer is defined by
+
+Ghost layer = f(patch) V box
+
+so moving in the dual space, we have
+
+Ghost layer = f(patch) V box
+            = f( f_inv(f(patch) V box) )
+            = f( patch V f_inv(box) )
+
+
+
+
+
+
+
+*/
+
+template<class Tvec, class TgridVec>
+void GenerateGhostLayer<Tvec, TgridVec>::_impl_evaluate_internal() {
+    auto edges = get_edges();
+
+    // inputs
+    auto &sim_box           = edges.sim_box.value;
+    auto &patch_data_layers = edges.patch_data_layers;
+    auto &patch_tree        = edges.patch_tree.get_patch_tree();
+
+    // outputs
+    auto &ghost_layers = edges.ghost_layers.patchdatas;
+
+    auto paving = get_paving(mode, sim_box);
+}
+
+template<class Tvec, class TgridVec>
+std::string GenerateGhostLayer<Tvec, TgridVec>::_impl_get_tex() {
+    return "TODO";
+}
