1ac85795d7108f754b54768a148af1e969b60efb
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-08-12 13:14:11 +0200

    pause until ddshared pr

---
+++ src/shammodels/ramses/include/shammodels/ramses/modules/GenGhostZoneLayers.hpp
@@ -0,0 +1,73 @@
+// -------------------------------------------------------//
+//
+// SHAMROCK code for hydrodynamics
+// Copyright (c) 2021-2025 Timothée David--Cléris <tim.shamrock@proton.me>
+// SPDX-License-Identifier: CeCILL Free Software License Agreement v2.1
+// Shamrock is licensed under the CeCILL 2.1 License, see LICENSE for more information
+//
+// -------------------------------------------------------//
+
+#pragma once
+
+/**
+ * @file GenGhostZoneLayers.hpp
+ * @author Timothée David--Cléris (tim.shamrock@proton.me)
+ * @brief Field variant object to instanciate a variant on the patch types
+ * @date 2023-07-31
+ */
+
+#include "shammodels/ramses/modules/FindGhostLayerCandidates.hpp"
+#include "shamrock/solvergraph/PatchDataLayerDDShared.hpp"
+
+namespace shammodels::basegodunov::modules {
+
+    template<class Tvec, class TgridVec>
+    class GenGhostZonesLayers : public shamrock::solvergraph::INode {
+
+        public:
+        GenGhostZonesLayers(GhostLayerGenMode mode) : mode(mode) {}
+
+        private:
+        GhostLayerGenMode mode;
+
+        struct Edges {
+            // inputs
+            const shamrock::solvergraph::ScalarEdge<shammath::AABB<TgridVec>> &sim_box;
+            const shamrock::solvergraph::PatchDataLayerRefs &patch_data_layers;
+            const shamrock::solvergraph::DDSharedScalar<GhostLayerCandidateInfos>
+                &ghost_layers_candidates;
+            const shamrock::solvergraph::ScalarsEdge<shammath::AABB<TgridVec>> &patch_boxes;
+            // outputs
+            shamrock::solvergraph::PatchDataLayerDDShared &ghost_zones_layers;
+        };
+
+        inline void set_edges(
+            std::shared_ptr<shamrock::solvergraph::ScalarEdge<shammath::AABB<TgridVec>>> sim_box,
+            std::shared_ptr<shamrock::solvergraph::PatchDataLayerRefs> patch_data_layers,
+            std::shared_ptr<shamrock::solvergraph::DDSharedScalar<GhostLayerCandidateInfos>>
+                ghost_layers_candidates,
+            std::shared_ptr<shamrock::solvergraph::ScalarsEdge<shammath::AABB<TgridVec>>>
+                patch_boxes,
+            std::shared_ptr<shamrock::solvergraph::PatchDataLayerDDShared> ghost_zones_layers) {
+            __internal_set_ro_edges(
+                {sim_box, patch_data_layers, ghost_layers_candidates, patch_boxes});
+            __internal_set_rw_edges({ghost_zones_layers});
+        }
+
+        inline Edges get_edges() {
+            return Edges{
+                get_ro_edge<shamrock::solvergraph::ScalarEdge<shammath::AABB<TgridVec>>>(0),
+                get_ro_edge<shamrock::solvergraph::PatchDataLayerRefs>(1),
+                get_ro_edge<shamrock::solvergraph::DDSharedScalar<GhostLayerCandidateInfos>>(2),
+                get_ro_edge<shamrock::solvergraph::ScalarsEdge<shammath::AABB<TgridVec>>>(3),
+                get_rw_edge<shamrock::solvergraph::PatchDataLayerDDShared>(0),
+            };
+        }
+
+        void _impl_evaluate_internal();
+
+        inline virtual std::string _impl_get_label() { return "GenGhostZonesLayers"; };
+
+        virtual std::string _impl_get_tex();
+    };
+} // namespace shammodels::basegodunov::modules
---
+++ src/shammodels/ramses/src/modules/GenGhostZonesLayers.cpp
@@ -0,0 +1,58 @@
+// -------------------------------------------------------//
+//
+// SHAMROCK code for hydrodynamics
+// Copyright (c) 2021-2025 Timothée David--Cléris <tim.shamrock@proton.me>
+// SPDX-License-Identifier: CeCILL Free Software License Agreement v2.1
+// Shamrock is licensed under the CeCILL 2.1 License, see LICENSE for more information
+//
+// -------------------------------------------------------//
+
+/**
+ * @file GenGhostZonesLayers.cpp
+ * @author Timothée David--Cléris (tim.shamrock@proton.me)
+ * @brief
+ */
+
+#include "shambase/assert.hpp"
+#include "shambase/exception.hpp"
+#include "shambackends/DeviceBuffer.hpp"
+#include "shammath/AABB.hpp"
+#include "shammath/paving_function.hpp"
+#include "shammodels/ramses/modules/FindGhostLayerCandidates.hpp"
+#include "shammodels/ramses/modules/GenGhostZoneLayers.hpp"
+#include "shamsys/NodeInstance.hpp"
+#include <stdexcept>
+
+template<class Tvec, class TgridVec>
+void shammodels::basegodunov::modules::GenGhostZonesLayers<Tvec, TgridVec>::
+    _impl_evaluate_internal() {
+    auto edges = get_edges();
+
+    // inputs
+    auto &sim_box                 = edges.sim_box.value;
+    auto &patch_data_layers       = edges.patch_data_layers;
+    auto &ghost_layers_candidates = edges.ghost_layers_candidates;
+    auto &patch_boxes             = edges.patch_boxes;
+
+    // outputs
+    auto &ghost_zones_layers = edges.ghost_zones_layers;
+
+    shambase::DistributedDataShared<sham::DeviceBuffer<u32>> idx_in_ghost;
+
+    auto sched = shamsys::instance::get_compute_scheduler_ptr();
+
+    // map candidates to indexes in ghosts
+    ghost_layers_candidates.values.map<sham::DeviceBuffer<u32>>(
+        [&](u64 sender, u64 receiver, &GhostLayerCandidateInfos &infos) -> sham::DeviceBuffer<u32> {
+            sham::DeviceBuffer<u32> ret(0, sched);
+
+            return ret;
+        });
+}
+
+template<class Tvec, class TgridVec>
+std::string shammodels::basegodunov::modules::GenGhostZonesLayers<Tvec, TgridVec>::_impl_get_tex() {
+    return "TODO";
+}
+
+template class shammodels::basegodunov::modules::GenGhostZonesLayers<f64_3, i64_3>;
