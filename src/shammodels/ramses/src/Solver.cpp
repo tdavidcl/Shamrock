9c62454153dd7e2dfbbb170ae3bdc3bbb387cfe7
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-08-15 16:04:31 +0200

    fix

--- src/shammodels/ramses/src/Solver.cpp
+++ src/shammodels/ramses/src/Solver.cpp
@@ -929,6 +929,7 @@ void shammodels::basegodunov::Solver<Tvec, TgridVec>::evolve_once() {
     }
 
     // give to the solvergraph the patch rank owners
+    storage.patch_rank_owner->values = {};
     scheduler().for_each_global_patch([&](const shamrock::patch::Patch p) {
         storage.patch_rank_owner->values.add_obj(
             p.id_patch, scheduler().get_patch_rank_owner(p.id_patch));
