66301556dd3021c2989ce7f2dbea85f4fd5854f9
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-08-18 12:45:53 +0200

    tmp

---
+++ src/shammodels/ramses/src/modules/ExtractGhostLayer.cpp
@@ -0,0 +1,64 @@
+// -------------------------------------------------------//
+//
+// SHAMROCK code for hydrodynamics
+// Copyright (c) 2021-2025 Timothée David--Cléris <tim.shamrock@proton.me>
+// SPDX-License-Identifier: CeCILL Free Software License Agreement v2.1
+// Shamrock is licensed under the CeCILL 2.1 License, see LICENSE for more information
+//
+// -------------------------------------------------------//
+
+/**
+ * @file ExtractGhostLayer.cpp
+ * @author Timothée David--Cléris (tim.shamrock@proton.me)
+ * @brief
+ */
+
+#include "shambase/assert.hpp"
+#include "shambase/exception.hpp"
+#include "shambackends/DeviceBuffer.hpp"
+#include "shambackends/DeviceScheduler.hpp"
+#include "shammath/AABB.hpp"
+#include "shammath/paving_function.hpp"
+#include "shammodels/ramses/modules/ExtractGhostLayer.hpp"
+#include "shammodels/ramses/modules/FindGhostLayerCandidates.hpp"
+#include "shamrock/patch/PatchDataField.hpp"
+#include "shamrock/patch/PatchDataLayer.hpp"
+#include "shamsys/NodeInstance.hpp"
+#include <stdexcept>
+
+void shammodels::basegodunov::modules::ExtractGhostLayer::_impl_evaluate_internal() {
+    StackEntry stack_loc{};
+
+    auto edges = get_edges();
+
+    // inputs
+    auto &patch_data_layers = edges.patch_data_layers;
+    auto &idx_in_ghost      = edges.idx_in_ghost;
+
+    // outputs
+    auto &ghost_layer = edges.ghost_layer;
+
+    auto dev_sched       = shamsys::instance::get_compute_scheduler_ptr();
+    sham::DeviceQueue &q = shambase::get_check_ref(dev_sched).get_queue();
+
+    // extract the ghost layers
+    auto idx_in_ghost_it = idx_in_ghost.buffers.begin();
+
+    // iterate on both DDShared containers
+    for (; idx_in_ghost_it != idx_in_ghost.buffers.end(); ++idx_in_ghost_it) {
+
+        auto [sender, receiver] = idx_in_ghost_it->first;
+
+        const sham::DeviceBuffer<u32> &sender_idx_in_ghost = idx_in_ghost_it->second;
+
+        shamrock::patch::PatchDataLayer ghost_zone(ghost_layer_layout);
+
+        // extract the actual data
+        patch_data_layers.get(sender).append_subset_to(
+            sender_idx_in_ghost, u32(sender_idx_in_ghost.get_size()), ghost_zone);
+
+        ghost_layer.patchdatas.add_obj(sender, receiver, std::move(ghost_zone));
+    }
+}
+
+std::string shammodels::basegodunov::modules::ExtractGhostLayer::_impl_get_tex() { return "TODO"; }
---
+++ src/shamrock/include/shamrock/solvergraph/PatchDataLayerEdgeToRefs.hpp
@@ -0,0 +1,74 @@
+// -------------------------------------------------------//
+//
+// SHAMROCK code for hydrodynamics
+// Copyright (c) 2021-2025 Timothée David--Cléris <tim.shamrock@proton.me>
+// SPDX-License-Identifier: CeCILL Free Software License Agreement v2.1
+// Shamrock is licensed under the CeCILL 2.1 License, see LICENSE for more information
+//
+// -------------------------------------------------------//
+
+#pragma once
+
+/**
+ * @file PatchDataLayerEdgeToRefs.hpp
+ * @author Timothée David--Cléris (tim.shamrock@proton.me)
+ * @brief Defines the PatchDataLayerEdgeToRefs class for converting patch data layer edges to
+ * references.
+ *
+ */
+
+#include "shamrock/solvergraph/INode.hpp"
+#include "shamrock/solvergraph/PatchDataLayerEdge.hpp"
+#include "shamrock/solvergraph/PatchDataLayerRefs.hpp"
+
+namespace shamrock::solvergraph {
+
+    class PatchDataLayerEdgeToRefs : public INode {
+
+        public:
+        PatchDataLayerEdgeToRefs() {}
+
+        /// Utility struct to list the refs to the edges
+        struct Edges {
+            PatchDataLayerEdge &original;
+            PatchDataLayerRefs &refs;
+        };
+
+        /**
+         * @brief Set the edges of the node
+         *
+         * Set the edge that will be freed by this node
+         *
+         * @param to_free The node to free
+         */
+        inline void set_edges(
+            std::shared_ptr<PatchDataLayerEdge> original,
+            std::shared_ptr<PatchDataLayerRefs> refs) {
+            __internal_set_ro_edges({});
+            __internal_set_rw_edges({original, refs});
+        }
+
+        /// Get the edges of the node
+        inline Edges get_edges() {
+            return Edges{get_rw_edge<PatchDataLayerEdge>(0), get_rw_edge<PatchDataLayerRefs>(1)};
+        }
+
+        /// Evaluate the node
+        inline void _impl_evaluate_internal() {
+            StackEntry stack_loc{};
+
+            auto edges = get_edges();
+            edges.refs.patchdatas
+                = edges.original.patchdatas.map<std::reference_wrapper<patch::PatchDataLayer>>(
+                    [](u64 id_patch, patch::PatchDataLayer &pdat) {
+                        return std::ref(pdat);
+                    });
+        }
+
+        /// Get the label of the node
+        inline virtual std::string _impl_get_label() { return "PatchDataLayerEdgeToRefs"; };
+
+        /// Get the TeX representation of the node
+        inline virtual std::string _impl_get_tex() { return "TODO"; }
+    };
+} // namespace shamrock::solvergraph
