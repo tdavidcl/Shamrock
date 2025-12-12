ce58e5a0c0df104706a0813f177e751035385640
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-12-12 14:20:13 +0100

    [CI] try to trigger update authors in pre-commit.ci

--- src/shammodels/sph/src/pySPHModel.cpp
+++ src/shammodels/sph/src/pySPHModel.cpp
@@ -10,7 +10,6 @@
 /**
  * @file pySPHModel.cpp
  * @author David Fang (david.fang@ikmail.com)
- * @author Timothée David--Cléris (tim.shamrock@proton.me)
  * @author Yona Lapeyre (yona.lapeyre@ens-lyon.fr)
  * @brief
  */
