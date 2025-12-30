21d48f353a5a9483fd49054396a798c41719abb3
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-12-30 11:44:53 +0100

    better

--- src/pylib/shamrock/__init__.py
+++ src/pylib/shamrock/__init__.py
@@ -1,8 +1,11 @@
 try:
-    from .pyshamrock import *
-except ImportError:
-    # the one above will fail if pyshamrock is declared by the embed python interpreter
-    # so we try to import it from the global namespace
+    # try to import from the global namespace (works if embedded python interpreter is used)
     from pyshamrock import *
-
+    IMPORT_LOG = "global"
+except ImportError:
+    # then it is a library mode, we import from the local namespace
+    from .pyshamrock import *
+    IMPORT_LOG = "local"
+    
 print(f"pyshamrock imported from {__file__}")
+print(f"import log: {IMPORT_LOG}")
