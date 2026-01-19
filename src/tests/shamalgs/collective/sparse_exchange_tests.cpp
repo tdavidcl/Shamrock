99282cfba22be75fa3133db21e085922a7e48ee6
Timothée David--Cléris <timothee.davidcleris@proton.me>
2026-01-19 20:38:29 +0100

    fixed ?

--- src/tests/shamalgs/collective/sparse_exchange_tests.cpp
+++ src/tests/shamalgs/collective/sparse_exchange_tests.cpp
@@ -14,6 +14,7 @@
 #include "shamcomm/logs.hpp"
 #include "shamsys/NodeInstance.hpp"
 #include "shamtest/shamtest.hpp"
+#include <fmt/std.h>
 #include <vector>
 
 namespace {
@@ -32,27 +33,20 @@ void reorder_msg(std::vector<TestElement> &test_elements) {
     });
 }
 
-void test_sparse_exchange(std::vector<TestElement> test_elements, size_t max_alloc_size) {
-    auto dev_sched = shamsys::instance::get_compute_scheduler_ptr();
-
-    reorder_msg(test_elements);
-
-    std::vector<sham::DeviceBuffer<u8>> all_bufs;
-
-    std::mt19937 eng(0x123);
-    for (const auto &test_element : test_elements) {
-        all_bufs.push_back(
-            shamalgs::random::mock_buffer_usm<u8>(dev_sched, eng(), test_element.size));
-    }
+#if false
+void validate_comm_table(
+    const std::vector<TestElement> &test_elements,
+    const shamalgs::collective::CommTable &comm_table,
+    size_t max_alloc_size) {
 
     std::vector<shamalgs::collective::CommMessageInfo> messages_send;
 
     std::vector<size_t> total_send_sizes = {0};
     std::vector<size_t> total_recv_sizes = {0};
-    {
+    shamalgs::collective::sequentialize([&]() {
         u32 send_buf_id    = 0;
-        size_t send_offset = 0;
         u32 recv_buf_id    = 0;
+        size_t send_offset = 0;
         size_t recv_offset = 0;
         for (u32 i = 0; i < test_elements.size(); i++) {
             if (test_elements[i].sender == shamcomm::world_rank()) {
@@ -74,8 +68,7 @@ void test_sparse_exchange(std::vector<TestElement> test_elements, size_t max_all
                     test_elements[i].sender,
                     "->",
                     test_elements[i].receiver,
-                    ") data :",
-                    all_bufs[i].copy_to_stdvec());
+                    ")");
 
                 if (send_offset + test_elements[i].size > max_alloc_size) {
                     send_buf_id++;
@@ -84,7 +77,6 @@ void test_sparse_exchange(std::vector<TestElement> test_elements, size_t max_all
                 }
 
                 total_send_sizes.at(send_buf_id) += test_elements[i].size;
-                send_offset += test_elements[i].size;
             }
             if (test_elements[i].receiver == shamcomm::world_rank()) {
                 if (recv_offset + test_elements[i].size > max_alloc_size) {
@@ -94,30 +86,161 @@ void test_sparse_exchange(std::vector<TestElement> test_elements, size_t max_all
                 }
 
                 total_recv_sizes.at(recv_buf_id) += test_elements[i].size;
-                recv_offset += test_elements[i].size;
             }
+        }
+    });
+
+    REQUIRE_EQUAL(comm_table.send_total_sizes, total_send_sizes);
+    REQUIRE_EQUAL(comm_table.recv_total_sizes, total_recv_sizes);
+
+    shamalgs::collective::sequentialize([&]() {
+        size_t send_msg_idx = 0;
+        size_t recv_msg_idx = 0;
+        for (u32 i = 0; i < test_elements.size(); i++) {
+            if (test_elements[i].sender == shamcomm::world_rank()) {
+                REQUIRE_EQUAL(
+                    comm_table.messages_send[send_msg_idx].message_size, test_elements[i].size);
+                REQUIRE_EQUAL(
+                    comm_table.messages_send[send_msg_idx].rank_sender, test_elements[i].sender);
+                REQUIRE_EQUAL(
+                    comm_table.messages_send[send_msg_idx].rank_receiver,
+                    test_elements[i].receiver);
+
+                send_msg_idx++;
+            }
+            if (test_elements[i].receiver == shamcomm::world_rank()) {
+                REQUIRE_EQUAL(
+                    comm_table.messages_recv[recv_msg_idx].message_size, test_elements[i].size);
+                REQUIRE_EQUAL(
+                    comm_table.messages_recv[recv_msg_idx].rank_sender, test_elements[i].sender);
+                REQUIRE_EQUAL(
+                    comm_table.messages_recv[recv_msg_idx].rank_receiver,
+                    test_elements[i].receiver);
+
+                auto &ref_buf = all_bufs[i];
+                sham::DeviceBuffer<u8> recov(test_elements[i].size, dev_sched);
+                auto off_info = shambase::get_check_ref(
+                    comm_table.messages_recv[recv_msg_idx].message_bytebuf_offset_recv);
+                size_t begin = off_info.data_offset;
+                size_t end   = begin + test_elements[i].size;
+                shambase::get_check_ref(recv_bufs.at(off_info.buf_id))
+                    .copy_range(begin, end, recov);
+
+                logger::info_ln(
+                    "sparse exchange test",
+                    "rank :",
+                    shamcomm::world_rank(),
+                    "recv message : (",
+                    test_elements[i].sender,
+                    "->",
+                    test_elements[i].receiver,
+                    ") data :",
+                    recov.copy_to_stdvec());
+
+                REQUIRE_EQUAL(recov.copy_to_stdvec(), ref_buf.copy_to_stdvec());
+
+                recv_msg_idx++;
+            }
+            REQUIRE_EQUAL(comm_table.message_all[i].message_size, test_elements[i].size);
+            REQUIRE_EQUAL(comm_table.message_all[i].rank_sender, test_elements[i].sender);
+            REQUIRE_EQUAL(comm_table.message_all[i].rank_receiver, test_elements[i].receiver);
+        }
+    });
+}
+
+template<>
+struct fmt::formatter<shamalgs::collective::CommMessageBufOffset> {
+
+    template<typename ParseContext>
+    constexpr auto parse(ParseContext &ctx) {
+        return ctx.begin();
+    }
+
+    template<typename FormatContext>
+    auto format(shamalgs::collective::CommMessageBufOffset c, FormatContext &ctx) const {
+        return fmt::format_to(
+            ctx.out(), "Offset(buf_id : {}, data_offset : {})", c.buf_id, c.data_offset);
+    }
+};
+
+template<>
+struct fmt::formatter<shamalgs::collective::CommMessageInfo> {
+
+    template<typename ParseContext>
+    constexpr auto parse(ParseContext &ctx) {
+        return ctx.begin();
+    }
+
+    template<typename FormatContext>
+    auto format(shamalgs::collective::CommMessageInfo c, FormatContext &ctx) const {
+        return fmt::format_to(
+            ctx.out(),
+            "Info(size : {}, sender : {}, receiver : {}, offset_send : {}, offset_recv : {})",
+            c.message_size,
+            c.rank_sender,
+            c.rank_receiver,
+            c.message_bytebuf_offset_send,
+            c.message_bytebuf_offset_recv);
+    }
+};
+
+void print_comm_table(const shamalgs::collective::CommTable &comm_table) {
+    std::stringstream ss;
+    ss << shambase::format(
+        "messages_send : [\n    {}\n]\n", fmt::join(comm_table.messages_send, "\n    "));
+    ss << shambase::format(
+        "messages_recv : [\n    {}\n]\n", fmt::join(comm_table.messages_recv, "\n    "));
+    ss << shambase::format(
+        "message_all : [\n    {}\n]\n", fmt::join(comm_table.message_all, "\n    "));
+    ss << shambase::format("send_message_global_ids : {}\n", comm_table.send_message_global_ids);
+    ss << shambase::format("recv_message_global_ids : {}\n", comm_table.recv_message_global_ids);
+    ss << shambase::format("send_total_sizes : {}\n", comm_table.send_total_sizes);
+    ss << shambase::format("recv_total_sizes : {}\n", comm_table.recv_total_sizes);
+    logger::info_ln(
+        "sparse exchange test", "rank :", shamcomm::world_rank(), "comm table :", "\n" + ss.str());
+}
+#endif
+
+void test_sparse_exchange(std::vector<TestElement> test_elements, size_t max_alloc_size) {
+    auto dev_sched = shamsys::instance::get_compute_scheduler_ptr();
+
+    reorder_msg(test_elements);
+
+    std::vector<sham::DeviceBuffer<u8>> all_bufs;
+
+    std::mt19937 eng(0x123);
+    for (const auto &test_element : test_elements) {
+        all_bufs.push_back(
+            shamalgs::random::mock_buffer_usm<u8>(dev_sched, eng(), test_element.size));
+    }
+
+    std::vector<shamalgs::collective::CommMessageInfo> messages_send;
+
+    for (u32 i = 0; i < test_elements.size(); i++) {
+        if (test_elements[i].sender == shamcomm::world_rank()) {
+            messages_send.push_back(
+                shamalgs::collective::CommMessageInfo{
+                    test_elements[i].size,
+                    test_elements[i].sender,
+                    test_elements[i].receiver,
+                    std::nullopt,
+                    std::nullopt,
+                    std::nullopt,
+                });
         }
     }
 
     shamalgs::collective::CommTable comm_table
         = shamalgs::collective::build_sparse_exchange_table(messages_send, max_alloc_size);
 
-    REQUIRE_EQUAL(comm_table.send_total_sizes, total_send_sizes);
-    REQUIRE_EQUAL(comm_table.recv_total_sizes, total_recv_sizes);
+    // print_comm_table(comm_table);
 
-    // allocate send and receive bufs
+    // allocate send bufs
     std::vector<std::unique_ptr<sham::DeviceBuffer<u8>>> send_bufs{};
 
     for (size_t i = 0; i < comm_table.send_total_sizes.size(); i++) {
         send_bufs.push_back(
             std::make_unique<sham::DeviceBuffer<u8>>(comm_table.send_total_sizes[i], dev_sched));
-    }
-
-    std::vector<std::unique_ptr<sham::DeviceBuffer<u8>>> recv_bufs{};
-
-    for (size_t i = 0; i < comm_table.recv_total_sizes.size(); i++) {
-        recv_bufs.push_back(
-            std::make_unique<sham::DeviceBuffer<u8>>(comm_table.recv_total_sizes[i], dev_sched));
     }
 
     // push data to the comm buf
@@ -131,6 +254,14 @@ void test_sparse_exchange(std::vector<TestElement> test_elements, size_t max_all
         auto &dest   = shambase::get_check_ref(send_bufs.at(off_info.buf_id));
 
         source.copy_range_offset(0, source.get_size(), dest, off_info.data_offset);
+    }
+
+    // allocate recv bufs
+    std::vector<std::unique_ptr<sham::DeviceBuffer<u8>>> recv_bufs{};
+
+    for (size_t i = 0; i < comm_table.recv_total_sizes.size(); i++) {
+        recv_bufs.push_back(
+            std::make_unique<sham::DeviceBuffer<u8>>(comm_table.recv_total_sizes[i], dev_sched));
     }
 
     // do the comm
@@ -158,37 +289,44 @@ void test_sparse_exchange(std::vector<TestElement> test_elements, size_t max_all
         }
     }
 
-    // time to check
-
-    size_t send_msg_idx = 0;
-    size_t recv_msg_idx = 0;
-    for (u32 i = 0; i < test_elements.size(); i++) {
-        if (test_elements[i].sender == shamcomm::world_rank()) {
-            REQUIRE_EQUAL(
-                comm_table.messages_send[send_msg_idx].message_size, test_elements[i].size);
-            REQUIRE_EQUAL(
-                comm_table.messages_send[send_msg_idx].rank_sender, test_elements[i].sender);
-            REQUIRE_EQUAL(
-                comm_table.messages_send[send_msg_idx].rank_receiver, test_elements[i].receiver);
-
-            send_msg_idx++;
+    {
+        std::stringstream ss;
+        ss << "send bufs :\n";
+        for (size_t i = 0; i < send_bufs.size(); i++) {
+            ss << "buf " << i << " : " << shambase::format("{}", send_bufs[i]->copy_to_stdvec())
+               << "\n";
         }
+        ss << "recv bufs :\n";
+        for (size_t i = 0; i < recv_bufs.size(); i++) {
+            ss << "buf " << i << " : " << shambase::format("{}", recv_bufs[i]->copy_to_stdvec())
+               << "\n";
+        }
+        logger::info_ln("sparse exchange test", "rank :", shamcomm::world_rank(), ss.str());
+    }
+
+    // time to check
+    std::vector<sham::DeviceBuffer<u8>> recv_messages;
+
+    for (size_t i = 0; i < comm_table.messages_recv.size(); i++) {
+        auto msg_info        = comm_table.messages_recv[i];
+        size_t global_msg_id = comm_table.recv_message_global_ids[i];
+
+        auto off_info
+            = shambase::get_check_ref(comm_table.messages_recv[i].message_bytebuf_offset_recv);
+
+        sham::DeviceBuffer<u8> recov(test_elements[global_msg_id].size, dev_sched);
+
+        size_t begin = off_info.data_offset;
+        size_t end   = begin + test_elements[global_msg_id].size;
+        shambase::get_check_ref(recv_bufs.at(off_info.buf_id)).copy_range(begin, end, recov);
+        recv_messages.push_back(std::move(recov));
+    }
+
+    // validate
+    u32 recv_idx = 0;
+    for (size_t i = 0; i < test_elements.size(); i++) {
         if (test_elements[i].receiver == shamcomm::world_rank()) {
-            REQUIRE_EQUAL(
-                comm_table.messages_recv[recv_msg_idx].message_size, test_elements[i].size);
-            REQUIRE_EQUAL(
-                comm_table.messages_recv[recv_msg_idx].rank_sender, test_elements[i].sender);
-            REQUIRE_EQUAL(
-                comm_table.messages_recv[recv_msg_idx].rank_receiver, test_elements[i].receiver);
-
-            auto &ref_buf = all_bufs[i];
-            sham::DeviceBuffer<u8> recov(test_elements[i].size, dev_sched);
-            auto off_info = shambase::get_check_ref(
-                comm_table.messages_recv[recv_msg_idx].message_bytebuf_offset_recv);
-            size_t begin = off_info.data_offset;
-            size_t end   = begin + test_elements[i].size;
-            shambase::get_check_ref(recv_bufs.at(off_info.buf_id)).copy_range(begin, end, recov);
-
+            REQUIRE_EQUAL(recv_messages[recv_idx].copy_to_stdvec(), all_bufs[i].copy_to_stdvec());
             logger::info_ln(
                 "sparse exchange test",
                 "rank :",
@@ -198,15 +336,13 @@ void test_sparse_exchange(std::vector<TestElement> test_elements, size_t max_all
                 "->",
                 test_elements[i].receiver,
                 ") data :",
-                recov.copy_to_stdvec());
-
-            REQUIRE_EQUAL(recov.copy_to_stdvec(), ref_buf.copy_to_stdvec());
-
-            recv_msg_idx++;
+                recv_messages[recv_idx].copy_to_stdvec(),
+                "data ref :",
+                all_bufs[i].copy_to_stdvec(),
+                "valid :",
+                recv_messages[recv_idx].copy_to_stdvec() == all_bufs[i].copy_to_stdvec());
+            recv_idx++;
         }
-        REQUIRE_EQUAL(comm_table.message_all[i].message_size, test_elements[i].size);
-        REQUIRE_EQUAL(comm_table.message_all[i].rank_sender, test_elements[i].sender);
-        REQUIRE_EQUAL(comm_table.message_all[i].rank_receiver, test_elements[i].receiver);
     }
 }
 
