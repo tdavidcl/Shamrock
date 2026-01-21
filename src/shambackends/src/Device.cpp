245b0b9238b5de7d2bf9abeae5b4d87581a67f6a
Timothée David--Cléris <timothee.davidcleris@proton.me>
2026-01-21 12:44:32 +0100

    better

--- src/shambackends/src/Device.cpp
+++ src/shambackends/src/Device.cpp
@@ -273,6 +273,13 @@ namespace sham {
             }
         }
 
+        {// PCI id infos
+            #if defined(SYCL_EXT_INTEL_DEVICE_INFO) && SYCL_EXT_INTEL_DEVICE_INFO >= 5
+            FETCH_PROP(pci_address,  sycl::ext::intel::info::device::pci_address)
+            logger::raw_ln("pci address :", pci_address.value());
+        #endif
+        }
+
         return DeviceProperties{
             Vendor::UNKNOWN,         // We cannot determine the vendor
             get_device_backend(dev), // Query the backend based on the platform name
