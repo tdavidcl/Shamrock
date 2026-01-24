85750a9b944d016729ff0550a08a786ce85a4589
Timothée David--Cléris <timothee.davidcleris@proton.me>
2026-01-24 20:53:28 +0100

    better

--- src/pylib/shamrock/utils/analysis/__init__.py
+++ src/pylib/shamrock/utils/analysis/__init__.py
@@ -1,19 +1,3 @@
-import json
-import os
-
-import numpy as np
-
-import shamrock.sys
-
-try:
-    import matplotlib
-    import matplotlib.animation as animation
-    import matplotlib.pyplot as plt
-
-    _HAS_MATPLOTLIB = True
-except ImportError:
-    _HAS_MATPLOTLIB = False
-
 from .ColumnDensityPlot import ColumnDensityPlot
 from .PerfHistory import PerfHistory
 from .SliceDensityPlot import SliceDensityPlot
