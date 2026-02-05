5a10f3cef9bd05aa38593325335fa71cbdd29fb3
Timothée David--Cléris <timothee.davidcleris@proton.me>
2026-02-05 15:23:52 +0100

    [SPH] ensure that dust can only be enabled in experimental mode

--- src/shammodels/sph/include/shammodels/sph/SolverConfig.hpp
+++ src/shammodels/sph/include/shammodels/sph/SolverConfig.hpp
@@ -155,10 +155,15 @@ namespace shammodels::sph {
             bool is_not_none = bool(std::get_if<MonofluidTVI>(&current_mode))
                                || bool(std::get_if<MonofluidComplete>(&current_mode));
             if (is_not_none) {
-                ON_RANK_0(
-                    logger::warn_ln(
-                        "SPH::config",
-                        "Dust config != None is work in progress, use it at your own risk"));
+                if (!shamrock::are_experimental_features_allowed()) {
+                    shambase::throw_with_loc<std::runtime_error>(
+                        "Dust config != None is experimental");
+                } else {
+                    ON_RANK_0(
+                        logger::warn_ln(
+                            "SPH::config",
+                            "Dust config != None is work in progress, use it at your own risk"));
+                }
             }
         }
     };
