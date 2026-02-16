45e0c22a90454a2b39b759f35ebf393fc3afe322
David--Cléris Timothée <timothee.davidcleris@proton.me>
2026-02-16 23:19:48 +0100

    Apply suggestion from @nbrucy
    Co-authored-by: Noé Brucy <nbrucy@users.noreply.github.com>

--- doc/sphinx/examples/tests_ci/sod_tube_godunov_amr.py
+++ doc/sphinx/examples/tests_ci/sod_tube_godunov_amr.py
@@ -37,7 +37,7 @@ cfg.set_riemann_solver_hll()
 # cfg.set_slope_lim_none()
 # cfg.set_slope_lim_vanleer_f()
 # cfg.set_slope_lim_vanleer_std()
-# cfg.set_slope_lim_vanleer_sym()
+# Set the slope limiter (among "none", "vanleer_f", "vanleer_std", "vanleer_sym", "minmod"
 cfg.set_slope_limiter("minmod")
 cfg.set_face_time_interpolation(True)
 mass_crit = 0.0000001 * 5 * 2 * 1.2
