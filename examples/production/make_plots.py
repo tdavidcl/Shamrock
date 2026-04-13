598c9b27352bf3bdcca680dcc644dc898f8131be
Timothée David--Cléris <timothee.davidcleris@proton.me>
2026-04-13 15:44:46 +0200

    update

--- examples/production/make_plots.py
+++ examples/production/make_plots.py
@@ -4,6 +4,11 @@ cases = {
         "is_torque_free": False,
         "racc": 0.8,
     },
+    "racc=0.5": {
+        "path": "_to_trash/circular_disc_sink_False_0.5_10000000",
+        "is_torque_free": False,
+        "racc": 0.5,
+    },
     "racc=0.25": {
         "path": "_to_trash/circular_disc_sink_False_0.25_10000000",
         "is_torque_free": False,
@@ -14,6 +19,11 @@ cases = {
         "is_torque_free": True,
         "racc": 0.8,
     },
+    "racc=0.5 (torque-free)": {
+        "path": "_to_trash/circular_disc_sink_True_0.5_10000000",
+        "is_torque_free": True,
+        "racc": 0.5,
+    },
     "racc=0.25 (torque-free)": {
         "path": "_to_trash/circular_disc_sink_True_0.25_10000000",
         "is_torque_free": True,
@@ -21,20 +31,21 @@ cases = {
     },
 }
 
+import json
 import os
-
+import numpy as np
 import matplotlib.pyplot as plt
 from shamrock.utils.analysis import (
     AnalysisHelper,
 )
 
 
-def render_profiles(cases):
+def render_rho_profiles(cases):
     helpers = {}
     for key, case in cases.items():
-        helpers[case] = {}
-        helpers[case]["helper"] = AnalysisHelper(
-            analysis_folder=os.path.join(cases[case]["path"], "analysis", "plots"),
+        helpers[key] = {}
+        helpers[key]["helper"] = AnalysisHelper(
+            analysis_folder=os.path.join(cases[key]["path"], "analysis", "plots"),
             analysis_prefix="density_profile",
         )
 
@@ -44,23 +55,22 @@ def render_profiles(cases):
     profile_list_analysis_id1 = helpers["racc=0.8"]["list_analysis_id"]
 
     for iplot in profile_list_analysis_id1:
-        has_iplot_data = True
-        for k in helpers:
-            has_iplot_data = has_iplot_data and iplot in helpers[k]["list_analysis_id"]
-        if not has_iplot_data:
-            continue
 
+        ref_key = None
         data = {}
         for k in helpers:
-            data[k] = helpers[k]["helper"].load_analysis(iplot).item()
+            if iplot in helpers[k]["list_analysis_id"]:
+                data[k] = helpers[k]["helper"].load_analysis(iplot).item()
+                if ref_key is None:
+                    ref_key = k
 
-        time = data["racc=0.8"]["time"]
-        bin_edges_x1d = data["racc=0.8"]["bin_edges_x1d"]
+        time = data[ref_key]["time"]
+        bin_edges_x1d = data[ref_key]["bin_edges_x1d"]
         bin_center = (bin_edges_x1d[:-1] + bin_edges_x1d[1:]) / 2
 
         plt.figure(dpi=150)
         for k, v in data.items():
-            plt.plot(bin_center, v["histo_convolve"], label=k)
+            plt.plot(bin_center, v["rho_profile"], label=k)
 
         text = f"t = {time:0.3f}"
         from matplotlib.offsetbox import AnchoredText
@@ -70,11 +80,136 @@ def render_profiles(cases):
 
         plt.xscale("log")
         plt.yscale("log")
-        plt.ylim(1e-6, 1e-3)
-        plt.xlim(0.1, 10)
-        plt.legend(loc="upper left")
-        plt.savefig(f"_to_trash/compare_torque_free_sink_{iplot:07}.png")
+        #plt.ylim(1e-6, 1e-3)
+        #plt.xlim(0.1, 21)
+        plt.legend(loc="lower left")
+        plt.savefig(f"_to_trash/compare_torque_free_sink_rho_profile_{iplot:07}.png")
         plt.close()
 
 
-render_profiles(cases)
+def render_Lz_profiles(cases):
+    helpers = {}
+    for key, case in cases.items():
+        helpers[key] = {}
+        helpers[key]["helper"] = AnalysisHelper(
+            analysis_folder=os.path.join(cases[key]["path"], "analysis", "plots"),
+            analysis_prefix="density_profile",
+        )
+
+    for k in helpers:
+        helpers[k]["list_analysis_id"] = helpers[k]["helper"].get_list_analysis_id()
+
+    profile_list_analysis_id1 = helpers["racc=0.8"]["list_analysis_id"]
+
+    for iplot in profile_list_analysis_id1:
+
+        ref_key = None
+        data = {}
+        for k in helpers:
+            if iplot in helpers[k]["list_analysis_id"]:
+                data[k] = helpers[k]["helper"].load_analysis(iplot).item()
+                if ref_key is None:
+                    ref_key = k
+
+        time = data[ref_key]["time"]
+        bin_edges_x1d = data[ref_key]["bin_edges_x1d"]
+        bin_center = (bin_edges_x1d[:-1] + bin_edges_x1d[1:]) / 2
+
+        plt.figure(dpi=150)
+        for k, v in data.items():
+            plt.plot(bin_center, v["Lz_profile"], label=k)
+
+        text = f"t = {time:0.3f}"
+        from matplotlib.offsetbox import AnchoredText
+
+        anchored_text = AnchoredText(text, loc=1)
+        plt.gca().add_artist(anchored_text)
+
+        plt.xscale("log")
+        plt.yscale("log")
+        #plt.ylim(1e-6, 1e-3)
+        #plt.xlim(0.1, 21)
+        plt.legend(loc="lower left")
+        plt.savefig(f"_to_trash/compare_torque_free_sink_Lz_profile_{iplot:07}.png")
+        plt.close()
+
+
+def compare_acc_rate(cases):
+    curves = {}
+    for key, case in cases.items():
+        disc_mass_filename = os.path.join(cases[key]["path"], "analysis", "disc_mass.json")
+        
+        ret = {}
+        with open(disc_mass_filename, "r") as fp:
+            disc_mass_data = json.load(fp)["disc_mass"]
+            ret["t"] = [d["t"] for d in disc_mass_data]
+            ret["disc_mass"] = [d["disc_mass"] for d in disc_mass_data]
+
+            # time derivative of the disc mass
+            ret["disc_mass_rate"] = np.diff(ret["disc_mass"]) / np.diff(ret["t"])
+        curves[key] = ret
+
+
+    
+    plt.figure(dpi=150)
+    for k, v in curves.items():
+        plt.plot(v["t"], v["disc_mass"], label=k)
+
+    #plt.xscale("log")
+    #plt.yscale("log")
+    #plt.ylim(1e-6, 1e-3)
+    #plt.xlim(0.1, 21)
+    plt.legend(loc="lower left")
+    plt.savefig(f"_to_trash/compare_torque_free_sink_acc_rate_disc_mass.png")
+    plt.close()
+    
+    plt.figure(dpi=150)
+    for k, v in curves.items():
+        plt.plot(v["t"][1:], v["disc_mass_rate"], label=k)
+
+    #plt.xscale("log")
+    #plt.yscale("log")
+    #plt.ylim(1e-6, 1e-3)
+    #plt.xlim(0.1, 21)
+    plt.legend(loc="lower right")
+    plt.savefig(f"_to_trash/compare_torque_free_sink_acc_rate_diff.png")
+    plt.close()
+
+def compare_dsink_rate(cases):
+
+    def get_r(pos):
+        return np.linalg.norm(pos)
+
+    curves = {}
+    for key, case in cases.items():
+        disc_mass_filename = os.path.join(cases[key]["path"], "analysis", "sinks.json")
+        
+        ret = {}
+        with open(disc_mass_filename, "r") as fp:
+            sinks_dat = json.load(fp)["sinks"]
+            ret["t"] = [d["t"] for d in sinks_dat]
+            ret["r"] = [get_r(d["sinks"][0]["pos"]) for d in sinks_dat]
+
+        curves[key] = ret
+
+
+    
+    plt.figure(dpi=150)
+    for k, v in curves.items():
+        plt.plot(v["t"], v["r"], label=k)
+
+    #plt.xscale("log")
+    #plt.yscale("log")
+    #plt.ylim(1e-6, 1e-3)
+    #plt.xlim(0.1, 21)
+    plt.legend(loc="lower left")
+    plt.savefig(f"_to_trash/compare_torque_free_sink_r.png")
+    plt.close()
+    
+
+
+
+compare_dsink_rate(cases)
+compare_acc_rate(cases)
+render_rho_profiles(cases)
+render_Lz_profiles(cases)
