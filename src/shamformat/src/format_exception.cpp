912b640ef7773c5697fda5fe1683aef9ae1cb0d5
Timothée David--Cléris <timothee.davidcleris@proton.me>
2026-05-17 15:49:05 +0200

    bruh

--- src/shamformat/src/format_exception.cpp
+++ src/shamformat/src/format_exception.cpp
@@ -17,8 +17,8 @@
 
 namespace sham {
 
-    /// Internal function ptr handle (intentionally large name for linking)
-    format_except_builder_t internal_func_ptr_make_format_exception = nullptr;
+    /// Internal function ptr handle, static + internal var should be ok across shared libraries
+    static format_except_builder_t internal_func_ptr_make_format_exception = nullptr;
 
     sham::format_error make_format_exception(
         std::string_view function_call,
