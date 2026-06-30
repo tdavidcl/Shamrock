8d3c0bf9f118cec50a46acad54a97fb3ac8b5dd9
David--Cléris Timothée <timothee.davidcleris@proton.me>
2026-06-30 09:22:14 +0200

    [Solvergraph] allow addition of contraints to Solvergraph (#1900)
    Allow creation of a SolverGraph with constraints to it like:
    ```c++
    auto constrained = SolverGraph::with_constraint(SolverGraphConstraint{
        .name = "sph_solver_graph",
        .node_check = [](const std::shared_ptr<INode> &n) { return true; },
        .edge_check = [](const std::shared_ptr<IEdge> &e) { return true; },
    });
    ```
    This will be useful to check for exemple that all edges in the graph are
    serializable in json.

--- src/shamrock/include/shamrock/solvergraph/INode.hpp
+++ src/shamrock/include/shamrock/solvergraph/INode.hpp
@@ -123,6 +123,8 @@ namespace shamrock::solvergraph {
         inline std::string get_tex() { return _impl_get_tex(); };
         /// Get the TeX of the node partial
         inline std::string get_tex_partial() { return _impl_get_tex(); };
+        /// Get the label of the node
+        inline std::string get_label() const { return _impl_get_label(); };
 
         /// print the node info
         inline virtual std::string print_node_info() const {
