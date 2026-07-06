96f996f1d0db2d019f112659e9bc35cbc540c565
David--Cléris Timothée <timothee.davidcleris@proton.me>
2026-07-07 00:28:33 +0200

    Update examples/sph/run_dustysettle_tvi.py

--- examples/sph/run_dustysettle_tvi.py
+++ examples/sph/run_dustysettle_tvi.py
@@ -664,7 +664,7 @@ def analyse_and_plot(j):
     axs[1].set_yscale("log")
     # axs[1].set_ylim(1e-12, 2) # if you want the full range
     axs[1].set_ylim(1e-4, 1e-1)  # if you want to see the dust only
-    axs[1].set_xlim(-box, box)
+    axs[1].set_xlim(-2.2 * H, 2.2 * H)
 
     gas_handle = Line2D(
         [0],
