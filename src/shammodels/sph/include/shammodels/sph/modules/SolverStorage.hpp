73c591f062570326e8d69c1683bf5f0139ee47e7
David--Cléris Timothée <timothee.davidcleris@proton.me>
2026-08-11 02:46:24 +0200

    [SPH] migrate sinks to solvergraph sync edges (#2021)
    Approved-by: shamrock-code-admin <312975147+shamrock-code-admin@users.noreply.github.com>
    Merged-by: Mergify <37929162+mergify[bot]@users.noreply.github.com>

--- src/shammodels/sph/include/shammodels/sph/modules/SolverStorage.hpp
+++ src/shammodels/sph/include/shammodels/sph/modules/SolverStorage.hpp
@@ -20,7 +20,6 @@
 #include "shambase/stacktrace.hpp"
 #include "shambackends/vec.hpp"
 #include "shammodels/sph/BasicSPHGhosts.hpp"
-#include "shammodels/sph/SinkPartStruct.hpp"
 #include "shammodels/sph/SolverConfig.hpp"
 #include "shammodels/sph/solvergraph/NeighCache.hpp"
 #include "shamrock/scheduler/SerialPatchTree.hpp"
@@ -108,8 +107,6 @@ namespace shammodels::sph {
         Component<shamrock::ComputeField<Tscal>> old_dtepsilon;
         Component<shamrock::ComputeField<Tvec>> old_dtdeltav;
         Component<shamrock::ComputeField<Tscal>> old_ds_j_dt;
-
-        Component<std::vector<SinkParticle<Tvec>>> sinks;
 
         struct Timings {
             f64 interface = 0;
