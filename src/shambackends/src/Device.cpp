8987a651ad570136c45a4281c1f5f0a69c2eca8f
David--Cléris Timothée <timothee.davidcleris@proton.me>
2026-08-31 16:55:04 +0200

    [Backends] add make_ndrange utility (#2236)

--- src/shambackends/src/Device.cpp
+++ src/shambackends/src/Device.cpp
@@ -361,6 +361,7 @@ namespace sham {
             .global_mem_cache_size      = shambase::get_check_ref(global_mem_cache_size),
             .local_mem_size             = shambase::get_check_ref(local_mem_size),
             .max_compute_units          = shambase::get_check_ref(max_compute_units),
+            .max_work_group_size        = shambase::get_check_ref(max_work_group_size),
             .max_mem_alloc_size_dev     = max_alloc_dev,
             .max_mem_alloc_size_host    = max_alloc_host,
             // the SYCL standard returns the alignment in bits, we convert to bytes for convenience
