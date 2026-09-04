ac705b74e1071997909987e97f96e3c9b6e501f1
Timothée David--Cléris <timothee.davidcleris@proton.me>
2026-09-04 22:06:37 +0200

    stuff

--- examples/sph/run_dustydisc.py
+++ examples/sph/run_dustydisc.py
@@ -390,8 +390,12 @@ sim.analysis_modules_fast.append(perf_analysis)
 
 
 class MassAnalysis:
-    def __init__(self, model, analysis_folder, analysis_prefix):
+    def __init__(
+        self, model, analysis_folder, analysis_prefix, time_unit="yr", mass_unit="sol_mass"
+    ):
         self.model = model
+        self.time_unit = time_unit
+        self.mass_unit = mass_unit
 
         self.analysis_prefix = os.path.join(analysis_folder, analysis_prefix)
         self.plot_prefix = os.path.join(analysis_folder, "plot_" + analysis_prefix)
@@ -404,12 +408,15 @@ class MassAnalysis:
         solver_config = model.get_current_config().to_json()
         has_dust = not (solver_config["dust_config"]["mode"]["type"] == "none")
 
+        model_units = model.get_current_config().get_units()
+
         barycenter, disc_mass = shamrock.model_sph.analysisBarycenter(model=model).get_barycenter()
 
         mass_hist_new = {
             "time": self.model.get_time(),
             "disc_mass": disc_mass,
             "barycenter": barycenter,
+            "unit_system": model_units.to_json(),
         }
 
         if has_dust:
@@ -419,9 +426,7 @@ class MassAnalysis:
             drag_mode = solver_config["dust_config"]["drag_mode"]
 
             if "grains_sizes" in drag_mode:
-                model_units = model.get_current_config().get_units()
-                g_size = [s * model_units.to("m") for s in drag_mode["grains_sizes"]]
-                mass_hist_new["grains_sizes_si"] = g_size
+                mass_hist_new["grains_sizes"] = drag_mode["grains_sizes"]
 
         if shamrock.sys.world_rank() == 0:
             try:
@@ -446,6 +451,19 @@ class MassAnalysis:
 
         has_dust = any("dust_mass" in h for h in mass_hist["history"])
 
+        unit_system_json = next(
+            (h["unit_system"] for h in mass_hist["history"] if "unit_system" in h), None
+        )
+        if unit_system_json is not None:
+            model_units = shamrock.UnitSystem(**unit_system_json)
+            time_conv = model_units.to(self.time_unit)
+            mass_conv = model_units.to(self.mass_unit)
+            length_conv = model_units.to("m")
+        else:
+            time_conv = 1.0
+            mass_conv = 1.0
+            length_conv = 1.0
+
         def time_gradient(arr, axis=0):
             # np.gradient needs at least 2 samples along the gradient axis
             if t.shape[0] < 2:
@@ -455,8 +473,8 @@ class MassAnalysis:
         t = [h["time"] for h in mass_hist["history"]]
         disc_mass = [h["disc_mass"] for h in mass_hist["history"]]
 
-        t = np.array(t)
-        disc_mass = np.array(disc_mass)
+        t = np.array(t) * time_conv
+        disc_mass = np.array(disc_mass) * mass_conv
 
         result = {
             "t": t,
@@ -466,7 +484,7 @@ class MassAnalysis:
 
         if has_dust:
             dust_mass = [h["dust_mass"] for h in mass_hist["history"]]
-            dust_mass = np.array(dust_mass)
+            dust_mass = np.array(dust_mass) * mass_conv
             dust_mass_all = np.nansum(dust_mass, axis=-1)
             gas_mass = disc_mass - dust_mass_all
 
@@ -474,16 +492,25 @@ class MassAnalysis:
             result["dust_mass_all"] = dust_mass_all
             result["gas_mass"] = gas_mass
 
-            grains_sizes_si = next(
-                h["grains_sizes_si"] for h in mass_hist["history"] if "grains_sizes_si" in h
+            grains_sizes = next(
+                h["grains_sizes"] for h in mass_hist["history"] if "grains_sizes" in h
             )
-            result["grains_sizes_si"] = np.array(grains_sizes_si)
+            result["grains_sizes"] = np.array(grains_sizes) * length_conv
 
             result["d_dust_mass_dt"] = time_gradient(dust_mass)
             result["d_dust_mass_all_dt"] = time_gradient(dust_mass_all)
             result["d_gas_mass_dt"] = time_gradient(gas_mass)
 
         return result
+
+    @staticmethod
+    def _symlog_linthresh(*arrays):
+        """Pick a symlog linear threshold from the smallest data scale present."""
+        abs_vals = np.concatenate([np.abs(a).ravel() for a in arrays if a.size > 0])
+        abs_vals = abs_vals[np.isfinite(abs_vals) & (abs_vals > 0)]
+        if abs_vals.size == 0:
+            return 1e-10
+        return np.nanmax(abs_vals) * 1e-6
 
     def plot_history(self, close_plots=True, figsize=(8, 5), dpi=200):
         if not _HAS_MATPLOTLIB:
@@ -497,24 +524,36 @@ class MassAnalysis:
 
             t = mass_hist["t"]
 
+            mass_unit_text = self.mass_unit
+            if mass_unit_text == "sol_mass":
+                mass_unit_text = "sol mass"
+
             plt.figure(figsize=figsize, dpi=dpi)
             plt.plot(t, mass_hist["disc_mass"], "+-")
-            plt.xlabel("t [code unit] (simulation)")
-            plt.ylabel("total mass [Sol mass] (real time)")
+            plt.xlabel(f"t [{self.time_unit}]")
+            plt.ylabel(f"total mass [{mass_unit_text}]")
             plt.savefig(self.plot_filename + "_total_mass.png")
             if close_plots:
                 plt.close()
 
+            if mass_hist["d_disc_mass_dt"].size > 0:
+                plt.figure(figsize=figsize, dpi=dpi)
+                plt.plot(t, mass_hist["d_disc_mass_dt"], "+-")
+                plt.xlabel(f"t [{self.time_unit}]")
+                plt.ylabel(rf"$dM/dt$ [{mass_unit_text} / {self.time_unit}]")
+                plt.yscale("symlog", linthresh=self._symlog_linthresh(mass_hist["d_disc_mass_dt"]))
+                plt.savefig(self.plot_filename + "_total_mass_dot.png")
+                if close_plots:
+                    plt.close()
+
             if "dust_mass" in mass_hist:
                 ndust = mass_hist["dust_mass"].shape[-1]
 
-                grains_sizes_si = mass_hist["grains_sizes_si"]
+                grains_sizes = mass_hist["grains_sizes"]
 
                 dust_cmap = plt.colormaps["plasma"]
-                dust_norm = mcolors.LogNorm(
-                    vmin=grains_sizes_si.min(), vmax=grains_sizes_si.max() * 10
-                )
-                dust_colors = dust_cmap(dust_norm(grains_sizes_si))
+                dust_norm = mcolors.LogNorm(vmin=grains_sizes.min(), vmax=grains_sizes.max() * 10)
+                dust_colors = dust_cmap(dust_norm(grains_sizes))
 
                 fig = plt.figure(figsize=figsize, dpi=dpi)
                 ax = fig.gca()
@@ -524,8 +563,8 @@ class MassAnalysis:
                 for i in range(ndust):
                     ax.plot(t, mass_hist["dust_mass"][:, i], "+-", color=dust_colors[i])
 
-                ax.set_xlabel("t [code unit] (simulation)")
-                ax.set_ylabel("mass [Sol mass] (real time)")
+                ax.set_xlabel(f"t [{self.time_unit}]")
+                ax.set_ylabel(f"mass [{mass_unit_text}]")
                 ax.set_yscale("log")
                 handles, labels = ax.get_legend_handles_labels()
                 add_cmap_legend_entry(
@@ -545,6 +584,57 @@ class MassAnalysis:
                 fig.savefig(self.plot_filename + "_masses.png")
                 if close_plots:
                     plt.close(fig)
+
+                if mass_hist["d_dust_mass_dt"].size > 0:
+                    fig = plt.figure(figsize=figsize, dpi=dpi)
+                    ax = fig.gca()
+                    ax.plot(t, mass_hist["d_disc_mass_dt"], "+-", color="0.0", label=r"$\dot{M}$")
+                    ax.plot(
+                        t,
+                        mass_hist["d_gas_mass_dt"],
+                        "+-",
+                        color="cornflowerblue",
+                        label=r"$\dot{M}_{\rm gas}$",
+                    )
+                    ax.plot(
+                        t,
+                        mass_hist["d_dust_mass_all_dt"],
+                        "+-",
+                        color="0.5",
+                        label=r"$\dot{M}_{\rm dust}$",
+                    )
+                    for i in range(ndust):
+                        ax.plot(t, mass_hist["d_dust_mass_dt"][:, i], "+-", color=dust_colors[i])
+
+                    ax.set_xlabel(f"t [{self.time_unit}]")
+                    ax.set_ylabel(rf"$dM/dt$ [{mass_unit_text} / {self.time_unit}]")
+                    ax.set_yscale(
+                        "symlog",
+                        linthresh=self._symlog_linthresh(
+                            mass_hist["d_disc_mass_dt"],
+                            mass_hist["d_gas_mass_dt"],
+                            mass_hist["d_dust_mass_all_dt"],
+                            mass_hist["d_dust_mass_dt"],
+                        ),
+                    )
+                    handles, labels = ax.get_legend_handles_labels()
+                    add_cmap_legend_entry(
+                        ax,
+                        dust_cmap,
+                        label=r"$\dot{M}_{\rm dust}(s_{\rm grain})$",
+                        extra_handles=handles,
+                        extra_labels=labels,
+                        loc="best",
+                    )
+
+                    dust_sm = cm.ScalarMappable(cmap=dust_cmap, norm=dust_norm)
+                    dust_sm.set_array([])
+                    cbar = fig.colorbar(dust_sm, ax=ax)
+                    cbar.set_label("grain size [m]")
+
+                    fig.savefig(self.plot_filename + "_masses_dot.png")
+                    if close_plots:
+                        plt.close(fig)
 
 
 mass_analysis = MassAnalysis(model, analysis_folder, "mass_history")
